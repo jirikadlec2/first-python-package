2066190be37ce29ec782fba5bb0980786e0536e2
jirikadlec2 <jirikadlec2@gmail.com>
2019-09-10 18:25:01 +0200

    removed unused modules.

--- FirstPythonPackage/module-a.py
+++
@@ -1,9 +0,0 @@
-import module-b
-
-def do_stuff():
-    final_result = module-b.run_helper("default_options")
-    if final_result:
-        print("module-a, do_stuff completed successfully!")
-    else:
-        print("error calculating result!")
-
