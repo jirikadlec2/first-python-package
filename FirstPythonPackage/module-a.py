e38fdb4269fa8f13c550978762dd529a0ef214b7
jirikadlec2 <jirikadlec2@gmail.com>
2019-09-10 18:00:55 +0200

    reorganizing package.

---
+++ FirstPythonPackage/module-a.py
@@ -0,0 +1,6 @@
+import module-b
+
+def do_stuff():
+    module-b.run_helper("default_options")
+    print("module-a, do_stuff completed successfully!")
+
---
+++ FirstPythonPackage/module-b.py
@@ -0,0 +1,2 @@
+def run_helper(options):
+    print("module-b, run_helper completed with options: " + options)
