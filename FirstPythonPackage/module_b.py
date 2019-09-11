b6679de3e760ca9d2b89ee0521529833344b092c
jirikadlec2 <jirikadlec2@gmail.com>
2019-09-11 09:00:21 +0200

    Fixed np array result in module_b.

--- FirstPythonPackage/module_b.py
+++ FirstPythonPackage/module_b.py
@@ -4,4 +4,4 @@ def run_helper(options):
     my_data = np.array([[1, 2],[2, 1]])
     my_result = my_data * 2
     print("module_b, run_helper completed with options: " + options)
-    return my_result
+    return np.amax(my_result)
